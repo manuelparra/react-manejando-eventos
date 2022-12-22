e375a1bcbb41f1e6a9ff034c9954559bb89338c9
Manuel Parra <manuel@macbook-air.local>
2022-12-22 13:28:42 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Button = () => {
+  const handleClick = () => alert('¡Me cliqueaste!');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button onClick={handleClick}>
+      Cliquéame
+    </button>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <Button />
+    </>
+  )
 }
 
 export default App;
